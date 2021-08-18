0a971fdaa9cff97958b71f4145248c02009b49d6
kimmyphua <kimberlyphuaweyhan@gmail.com>
2021-08-18 15:43:19 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'code_swag',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
